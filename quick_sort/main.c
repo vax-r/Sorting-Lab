a6851c4afc288d85f54ee5a06a36c8a4811809ee
vax-r <richard120310@gmail.com>
2024-03-04 14:26:34 +0800

    Introduce linux kernel list API in quick sort
    In the previous implementation of quick sort and related functions, we
    use a self-defined structure "node_t" and implement its related
    functions to do the list manipulation.
    
    Now we introduce the linux kernel linked list which is also called
    "struct list_head" in the project and replace all the related functions
    by linux kernel list api to enforce a stronger consistancy and more
    elegance implementation.

--- quick_sort/main.c
+++ quick_sort/main.c
@@ -5,54 +5,71 @@
 #include <stdlib.h>
 #include <time.h>
 
+#include "list.h"
+
 typedef struct __node {
-    struct __node *left, *right;
-    struct __node *next;
     long value;
+    struct list_head list;
 } node_t;
 
 
 /* List manipulation */
-void list_add(node_t **list, node_t *node_t)
+int list_length(struct list_head *head)
 {
-    node_t->next = *list;
-    *list = node_t;
+    if (!head)
+        return 0;
+
+    int size = 0;
+    struct list_head *cur;
+    list_for_each (cur, head)
+        size++;
+    return size;
 }
 
-node_t *list_tail(node_t **left)
+/* Create an empty list */
+struct list_head *list_new()
 {
-    while ((*left) && (*left)->next)
-        left = &((*left)->next);
-    return *left;
+    struct list_head *new_head = malloc(sizeof(struct list_head));
+    if (!new_head)
+        return NULL;
+    INIT_LIST_HEAD(new_head);
+    return new_head;
 }
 
-int list_length(node_t **left)
+bool list_insert_head(struct list_head *head, int n)
 {
-    int n = 0;
-    while (*left) {
-        ++n;
-        left = &((*left)->next);
+    if (!head)
+        return false;
+    node_t *new_node = malloc(sizeof(node_t));
+    if (!new_node)
+        return false;
+    INIT_LIST_HEAD(&new_node->list);
+    new_node->value = n;
+    list_add(&new_node->list, head);
+    return true;
+}
+
+node_t *list_remove_head(struct list_head *head)
+{
+    if (!head || list_empty(head))
+        return NULL;
+
+    node_t *f = list_first_entry(head, node_t, list);
+    list_del(&f->list);
+    return f;
+}
+
+void list_free(struct list_head *head)
+{
+    if (!head)
+        return;
+    node_t *entry, *safe;
+    list_for_each_entry_safe (entry, safe, head, list) {
+        free(entry);
     }
-    return n;
-}
-
-node_t *list_construct(node_t *list, int n)
-{
-    node_t *node = malloc(sizeof(node_t));
-    node->next = list;
-    node->value = n;
-    return node;
-}
-
-void list_free(node_t **list)
-{
-    node_t *node = (*list)->next;
-    while (*list) {
-        free(*list);
-        *list = node;
-        if (node)
-            node = node->next;
-    }
+    free(head);
+    head = NULL;
+    return;
 }
 
 /* End of list manipulation */
@@ -65,91 +82,120 @@ int count_level(int n)
     return i ? (i + 1) * 10 : 10;
 }
 
-void random_pivot(node_t **head)
+static inline void list_replace(struct list_head *old, struct list_head *new)
 {
-    int r = rand() % list_length(head);
-    node_t **cur = head;
-    for (int i = 0; i < r - 1 && (*cur)->next; i++)
-        cur = &((*cur)->next);
+    new->next = old->next;
+    new->next->prev = new;
+    new->prev = old->prev;
+    new->prev->next = new;
+}
 
-    node_t *tmp = (*cur)->next;
-    (*cur)->next = (*head);
-    (*head) = tmp;
+static inline void list_swap(struct list_head *entry1, struct list_head *entry2)
+{
+    struct list_head *pos = entry2->prev;
+
+    list_del(entry2);
+    list_replace(entry1, entry2);
+    if (pos == entry1)
+        pos = entry2;
+    list_add(entry1, pos);
+}
+
+void random_pivot(struct list_head *head)
+{
+    if (!head || list_is_singular(head))
+        return;
+    int r = rand() % list_length(head);
+    struct list_head *cur;
+    list_for_each (cur, head) {
+        if (r == 0)
+            break;
+        r--;
+    }
+    if (head->next != cur)
+        list_swap(head->next, cur);
 }
 
 /* Quick Sort */
-void quick_sort(node_t **list)
+void quick_sort(struct list_head **head)
 {
     clock_t time = clock();
 
-    int n = list_length(list);
-    int value;
+    if (!(*head) || list_empty(*head))
+        return;
+
+    int n = list_length(*head);
     int i = 0;
     int max_level = count_level(n);
 
-    node_t *begin[max_level], *end[max_level];
-    node_t *result = NULL, *left = NULL, *right = NULL;
+    struct list_head *begin[max_level];
+    begin[0] = *head;
+    for (int i = 1; i < max_level; i++)
+        begin[i] = list_new();
+    struct list_head *result = list_new();
+    struct list_head *left = list_new(), *right = list_new();
 
-    begin[0] = *list;
-    end[0] = list_tail(list);
-
-    int reach_i = i;
+    int deepest_level = i;
     while (i >= 0) {
-        node_t *L = begin[i], *R = end[i];
+        struct list_head *L = begin[i]->next, *R = begin[i]->prev;
         if (L != R) {
-            random_pivot(&L);
-            node_t *pivot = L;
-            value = pivot->value;
-            node_t *p = pivot->next;
-            pivot->next = NULL;
+            random_pivot(begin[i]);
+            node_t *pivot = list_remove_head(begin[i]);
 
-            while (p) {
-                node_t *n = p;
-                p = p->next;
-                list_add(n->value > value ? &right : &left, n);
+            node_t *entry, *safe;
+            list_for_each_entry_safe (entry, safe, begin[i], list) {
+                list_del(&entry->list);
+                if (entry->value > pivot->value) {
+                    list_add(&entry->list, right);
+                } else {
+                    list_add(&entry->list, left);
+                }
             }
 
-            begin[i] = left;
-            end[i] = list_tail(&left);
-            begin[i + 1] = pivot;
-            end[i + 1] = pivot;
-            begin[i + 2] = right;
-            end[i + 2] = list_tail(&right);
+            list_splice_init(left, begin[i]);
+            list_add(&pivot->list, begin[i + 1]);
+            list_splice_init(right, begin[i + 2]);
 
-            left = right = NULL;
             i += 2;
-            reach_i = reach_i < i ? i : reach_i;
+            deepest_level = deepest_level < i ? i : deepest_level;
         } else {
-            if (L)
-                list_add(&result, L);
+            if (list_is_singular(begin[i]))
+                list_splice_init(begin[i], result);
             i--;
         }
     }
-    *list = result;
+
+    for (int i = 0; i < max_level; i++)
+        list_free(begin[i]);
+    list_free(left);
+    list_free(right);
+
+    *head = result;
 
     time = clock() - time;
     printf("size : %d, max_level : %d, deepest level : %d, cpu clock : %ld\n",
-           n, max_level, reach_i, time);
+           n, max_level, deepest_level, time);
 }
 
 /* End of quick sort */
 
 
 /* Verify if list is order */
-static bool list_is_ordered(node_t *list)
+static bool list_is_ordered(struct list_head *head)
 {
-    bool first = true;
-    int value;
-    while (list) {
-        if (first) {
-            value = list->value;
-            first = false;
-        } else {
-            if (list->value < value)
-                return false;
-            value = list->value;
+    if (!head || list_empty(head))
+        return true;
+
+    struct list_head *cur;
+    list_for_each (cur, head) {
+        if (cur->next == head)
+            break;
+        node_t *a = list_entry(cur, node_t, list);
+        node_t *b = list_entry(cur->next, node_t, list);
+        if (a->value > b->value) {
+            printf("%ld, %ld\n", a->value, b->value);
+            return false;
         }
-        list = list->next;
     }
     return true;
 }
@@ -168,33 +214,31 @@ void shuffle(int *array, size_t n)
     }
 }
 
-void show_list(node_t **head)
+void show_list(struct list_head *head)
 {
-    while (*head) {
-        printf("%ld ", (*head)->value);
-        head = &((*head)->next);
-    }
+    node_t *cur;
+    list_for_each_entry (cur, head, list)
+        printf("%ld ", cur->value);
     printf("\n");
 }
 
 int main(int argc, char **argv)
 {
-    node_t *list = NULL;
-
     for (int i = 10; i <= 10000000; i *= 10) {
+        struct list_head *list = list_new();
         size_t count = i;
 
         int *test_arr = malloc(sizeof(int) * count);
 
         for (int i = 0; i < count; i++)
-            test_arr[i] = (count - i);
-        // shuffle(test_arr, count);
+            test_arr[i] = i;
+        shuffle(test_arr, count);
         while (count--)
-            list = list_construct(list, test_arr[count]);
+            list_insert_head(list, test_arr[count]);
 
         quick_sort(&list);
         assert(list_is_ordered(list));
-        list_free(&list);
+        list_free(list);
 
         free(test_arr);
     }
