3e2ac7ff2f9b48fa3a116dd27a22c56d41ef56a6
vax-r <richard120310@gmail.com>
2024-03-05 11:30:53 +0800

    LRU basic set up and testing program
    Build up basic implementation of LRU cache manipulation function and
    implement a testing program to test the result of the LRU cache in
    "main".
    
    We apply random sequence of put and get operation and check the most
    recently used element in LRU cache to determine whether the operation is
    done correctly.

---
+++ LRU/LRU.h
@@ -0,0 +1,87 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include "list.h"
+
+typedef struct {
+    int capacity;
+    int count;
+    struct list_head dhead;
+    struct hlist_head hhead[];
+} LRUCache;
+
+typedef struct {
+    int key;
+    int value;
+    struct hlist_node node;
+    struct list_head link;
+} LRUNode;
+
+LRUCache *lRUCacheCreate(int capacity)
+{
+    LRUCache *cache = malloc(2 * sizeof(int) + sizeof(struct list_head) +
+                             capacity * sizeof(struct hlist_head));
+    cache->capacity = capacity;
+    cache->count = 0;
+    INIT_LIST_HEAD(&cache->dhead);
+    for (int i = 0; i < capacity; i++)
+        INIT_HLIST_HEAD(&cache->hhead[i]);
+    return cache;
+}
+
+void lRUCacheFree(LRUCache *obj)
+{
+    struct list_head *pos, *n;
+    list_for_each_safe (pos, n, &obj->dhead) {
+        LRUNode *cache = list_entry(pos, LRUNode, link);
+        list_del(&cache->link);
+        free(cache);
+    }
+    free(obj);
+}
+
+int lRUCacheGet(LRUCache *obj, int key)
+{
+    int hash = key % obj->capacity;
+    struct hlist_node *pos;
+    hlist_for_each(pos, &obj->hhead[hash])
+    {
+        LRUNode *cache = list_entry(pos, LRUNode, node);
+        if (cache->key == key) {
+            list_move(&cache->link, &obj->dhead);
+            return cache->value;
+        }
+    }
+    return -1;
+}
+
+void lRUCachePut(LRUCache *obj, int key, int value)
+{
+    LRUNode *cache = NULL;
+    int hash = key % obj->capacity;
+    struct hlist_node *pos;
+    hlist_for_each(pos, &obj->hhead[hash])
+    {
+        LRUNode *c = list_entry(pos, LRUNode, node);
+        if (c->key == key) {
+            list_move(&c->link, &obj->dhead);
+            cache = c;
+        }
+    }
+
+    if (!cache) {
+        if (obj->count == obj->capacity) {
+            cache = list_last_entry(&obj->dhead, LRUNode, link);
+            list_move(&cache->link, &obj->dhead);
+            hlist_del(&cache->node);
+            hlist_add_head(&cache->node, &obj->hhead[hash]);
+        } else {
+            cache = malloc(sizeof(LRUNode));
+            hlist_add_head(&cache->node, &obj->hhead[hash]);
+            list_add(&cache->link, &obj->dhead);
+            obj->count++;
+        }
+        cache->key = key;
+    }
+    cache->value = value;
+}
---
+++ LRU/main.c
@@ -0,0 +1,49 @@
+#include <assert.h>
+#include <stdio.h>
+
+#include "LRU.h"
+
+/* Check whether the first entry is the most recently used node */
+bool checkLRU(LRUCache *obj, int key, int value)
+{
+    int hash = key % obj->capacity;
+    struct hlist_node *ptr = (&obj->hhead[hash])->first;
+    LRUNode *cache = list_entry(ptr, LRUNode, node);
+    if (cache->key != key || cache->value != value) {
+        printf("%d, %d\n", cache->key, cache->value);
+        return false;
+    }
+
+    cache = list_first_entry(&obj->dhead, LRUNode, link);
+    if (cache->key != key || cache->value != value) {
+        printf("here2\n");
+        return false;
+    }
+    return true;
+}
+
+int main(void)
+{
+    int size = 5;
+    LRUCache *cache = lRUCacheCreate(size);
+
+    int test_times = 100;
+    for (int i = 0; i < test_times; i++) {
+        int key = rand() % 10;
+        int value = rand() % 100;
+
+        if (rand() % 2) {
+            printf("put(%d, %d)\n", key, value);
+            lRUCachePut(cache, key, value);
+        } else {
+            printf("get(%d), ", key);
+            int result = lRUCacheGet(cache, key);
+            printf("Result: %d\n", result);
+        }
+
+        assert(checkLRU(cache, key, value));
+    }
+
+
+    return 0;
+}
